11902200aef825fd4a4df1addc2b14b89633fe8f
hussainraza22 <103336904+hussainraza22@users.noreply.github.com>
2023-08-27 22:47:19 +0500

    initial commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css';
+
+function Home() {
+  return (
+    <>
+        {/* <Navbar /> */}
+            hasnain
+        {/* <HeroSection /> */}
+
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/ServicesCard/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import 
+
+function ServicesCard(props) {
+  return (
+    <>
+        <div className='service-card'>
+            Man { props.name } Hu
+        </div>
+    </>
+
+  )
+}
+
+export default ServicesCard
